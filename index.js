4cba9f699d0e13c653532fce3631065ea7df4a3f
Javohir <108736263+java1777@users.noreply.github.com>
2025-12-12 19:04:46 +0500

    feat

---
+++ index.js
@@ -0,0 +1,51 @@
+// Masala - 1
+// let n = 12;
+// for (let i = 0; i <= n; i++) {
+//   if (n % i == 0) {
+//     console.log(i);
+//   }
+// }
+
+// Masala - 2
+// let n = 2;
+// let count = 0;
+// for (let i = 0; i <= n; i++) {
+//   if (n % i == 0) {
+//     count++;
+//   }
+// }
+// if (count % 2 == 0) {
+//   console.log("juft");
+// } else {
+//   console.log("toq");
+// }
+
+// Masala - 3;
+// let sum = 0;
+// for (let i = 0; i <= 12; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// Masala - 4
+// let count = 0;
+// for (let i = 1; i <= 13; i++) {
+//   if (i > 9 && i < 100) {
+//     count = count + 2;
+//   } else {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+
+// Masala - 5;
+let a = 0;
+for (let i = 0; i <= 18; i++) {
+  a += i;
+}
+if (a / 18 == 0) {
+  console.log("ha");
+} else {
+  console.log("yoq");
+}
